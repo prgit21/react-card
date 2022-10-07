f9f602515557fa65fe59a241409e69c9da1b9379
pr21 <pranav@aupplysail.com>
2022-10-07 09:39:28 +0530

    img err

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import './App.css';
 function App() {
   return (
     <div style={{margin: '50px'}}>
-    <Card title='Main Card' style={{width:'300px'}}>
-      Card render?
-    </Card>
+    <Card
+    
+    title='Obesity Childhood statistics' 
 
+    style={{width:'45%'}}>
+      <h2>1 in 3 </h2>
+        chikldren are at a risk of being overweight<br></br>
+   
+      
+
+      subcontent 2
+    </Card>
+    
+   
     </div>
   );
 }
--- src/Component/index.jsx
+++ src/Component/index.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './index.css'
+//import img1 from './Component/child.png'
 
 function Card({title,border,style,children}){
     return <div className= 'card' style = {style}>
     
     <div className='card-title'>
-      {title}
+      <h2>{title}</h2> 
 
     </div>
 
 
     <div className='body-children'>
-    {children}        
+    {children}  
+          
     
     </div>     
-    
+    <div className='row-two'>
+       {/* <img src={img1} alt=''/> */}
+        content of row 2
+
+    </div>
+
+    <div className='row-three'>
+        row 3
+    </div>
 
     </div>
 
