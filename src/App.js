70b9bebe04680392a615c77d3132e4db5c7e5dcc
pr21 <pranav@aupplysail.com>
2022-10-11 08:16:12 +0530

    added dependecnies for fa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 
 import Card from './Component/index'
 import './App.css';
+import {Box} from '@mui/material'
 
 function App() {
   return (
-    <div style={{margin: '50px'}}>
-    <Card
     
-    title='Obesity Childhood statistics' 
-
-    style={{width:'45%'}}>
-      <h2>1 in 3 </h2>
-        chikldren are at a risk of being overweight<br></br>
-   
+    <div className ='grey-box'style={{ margin: '50px' }}>
       
+      <Card
+          
+          title='Obesity Childhood statistics'
 
-      subcontent 2
-    </Card>
-    
-   
+          style={{ width: '45%' }}>
+          
+          <h2>1 in 3 </h2>
+          children are at a risk of being overweight<br></br>
+
+
+
+          subcontent 2
+        </Card>
+  
+
+{/* <div className='box-model' style= {{ marginLeft: '40%', marginTop: '60px', width: '30%' }}>
+    <Box color="white" bgcolor="palevioletred" p={1}>
+      Obesity in children
+      
+    </Box>
     </div>
+  */}
+
+
+
+
+      </div>
   );
 }
 
 export default App;
+
+//check fa library for icon
+//positioning css
+//use flex and set 50%
---
+++ src/Component/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './index.css'
+//import img1 from './Component/child.png'
+
+function Card({title,border,style,children}){
+    return <div className= 'card' style = {style}>
+
+
+    {/* <div className='fontawesome'>
+    <script src="https://kit.fontawesome.com/d9738e8468.js" crossorigin="anonymous"></script>
+    <h1>hello    <i class="fa-solid fa-user"></i></h1>
+</div> */}
+    
+    <div className='card-title'>
+      <h2>{title}</h2> 
+          
+        
+    </div>
+
+
+    <div className='body-children'>
+    {children}  
+          
+    
+    </div>     
+    <div className='row-two'>
+     {/*  <img src={img1} alt=''/> */}
+        content of row 2
+
+    </div>
+
+    <div className='row-three'>
+        row 3
+    </div>
+
+    </div>
+
+}
+
+export default Card;
